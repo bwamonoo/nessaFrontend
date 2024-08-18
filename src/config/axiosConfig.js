e4120bdc1fb21d9abd0fda66aa79894e22c246db
Amonoo <benjaminamonoowilberforce@outlook.com>
2024-08-18 21:32:53 +0000

    doneokk

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://787b-102-176-65-211.ngrok-free.app',
   withCredentials: true,
 });
 
