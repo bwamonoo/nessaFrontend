8bf3f7a52b1e9f480de3b7f85b3ca9217a8d3d72
Amonoo <benjaminamonoowilberforce@outlook.com>
2024-08-18 19:10:26 +0000

    completed,

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import Banner from '../components/Banner/Banner';
 import Footer from '../components/Footer/Footer';
 import ProductSection from '../components/ProductSection';
@@ -6,9 +6,16 @@ import '../styles/Home.css';
 import FirstHeader from '../components/Header/FirstHeader';
 import SecondHeader from '../components/Header/SecondHeader';
 import ThirdHeader from '../components/Header/ThirdHeader';
+import { NessaContext } from '../context/NessaContext';
 
 const Home = () => {
+  const { fetchCartItems, fetchWishlist } = useContext(NessaContext);
   const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    fetchCartItems();
+    fetchWishlist();
+  }, []);
 
   return (
     <div className="home-container">
