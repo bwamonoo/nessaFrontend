571800c6711004868971d6f064413089497493df
Amonoo <benjaminamonoowilberforce@outlook.com>
2024-08-19 17:04:51 +0000

    untitled

--- src/pages/ConfirmEmail.js
+++ src/pages/ConfirmEmail.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import '../styles/ConfirmEmail.css'
 
 const ConfirmEmail = () => {
 	const location = useLocation();
   const { message } = location.state || { message: "No message passed" };
 	return (
-		<div>
-			<p>Registration Success Page</p>
+		<div className='confirm-email-box'>
+			<span>Registration Success Page</span>
 			<p>{message}</p>
 		</div>
 	)
