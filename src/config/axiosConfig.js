16415ff7c4c18e28bfd844db9c04a8d2fa031bf0
Amonoo <benjaminamonoowilberforce@outlook.com>
2024-08-19 07:15:50 +0000

    edite base url for sending requests to backend again

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://d9e3-102-176-65-170.ngrok-free.app',
+  baseURL: 'http://localhost:8000',
   withCredentials: true,
 });
 
